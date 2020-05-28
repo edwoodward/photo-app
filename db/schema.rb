1fdb0e5e0b5bd023dd16f8afd36497e5dfb9afb6
edwoodward <ecw1@rice.edu>
2020-05-28 13:10:42 -0500

    Added image upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_24_192015) do
+ActiveRecord::Schema.define(version: 2020_05_27_012428) do
+
+  create_table "images", force: :cascade do |t|
+    t.string "name"
+    t.string "picture"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_images_on_user_id"
+  end
 
   create_table "payments", force: :cascade do |t|
     t.string "email"
